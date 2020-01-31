50d6e7b59ac82c1c510176cc43110b8a6edfae99
JamesCZ98 <james.coppock98@gmail.com>
2020-01-31 17:36:17 -0600

    Began RSA back-end

--- src/back_end/rsa.py
+++ src/back_end/rsa.py
@@ -1,6 +1,39 @@
 # rsa.py
+import math
+from random import random
+
+
+def is_prime(number):
+    if number % 2 == 0:
+        return False
+    for i in range(2, 10):
+        k = int(random() * number)
+        if math.gcd(number, k) != 1:
+            return False
+        if k ** (number - 1) % number != 1:
+            return False
+    return True
+
+def get_random_in_range(minimum, maximum):
+    return int(minimum + (random() * (maximum - minimum)))
+
+def get_prime(minimum, maximum):
+    p = get_random_in_range(minimum, maximum)
+    while not is_prime(p):
+        p = get_random_in_range(minimum, maximum)
+    return p
 
 
 class RSA:
+    minimum = 10000
+    maximum = 99999
+
     def __init__(self):
-        self.message = "Hello, world!"
+        self.p = get_prime(RSA.minimum, RSA.maximum)
+        self.q = get_prime(RSA.minimum, RSA.maximum)
+        self.n = self.p * self.q
+        self.f_of_n = (self.p - 1) * (self.q - 1)
+
+        self.e = get_random_in_range(1000, self.f_of_n)
+        while math.gcd(self.e, self.f_of_n) != 1:
+            self.e = get_random_in_range(1000, self.f_of_n)
--- src/front_end/application.py
+++ src/front_end/application.py
@@ -1,7 +1,9 @@
 # application.py
 
-from back_end.rsa import RSA
-
+from back_end.rsa import *
 
 rsa = RSA()
-print(rsa.message)
+
+print(rsa.p)  # large prime, p
+print(rsa.q)  # large prime, q
+print(rsa.e)  # e, part of public key
