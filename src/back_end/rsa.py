b3414b757f62969d97b24a4e26ec820e8afeace4
JamesCZ98 <james.coppock98@gmail.com>
2020-02-05 08:55:52 -0600

    Finished base RSA implementation

--- src/back_end/rsa.py
+++ src/back_end/rsa.py
@@ -3,6 +3,7 @@ import math
 from random import random
 
 
+# Checks if a given number is prime
 def is_prime(number: int) -> bool:
     if number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0 or number % 11 == 0:
         return False
@@ -15,10 +16,12 @@ def is_prime(number: int) -> bool:
     return True
 
 
+# Returns a random integer within the minimum and maximum, inclusively.
 def get_random_in_range(minimum: int, maximum: int) -> int:
     return int(minimum + (random() * (maximum - minimum)))
 
 
+# Finds a prime number within a range.
 def get_prime(minimum: int, maximum: int) -> int:
     p = get_random_in_range(minimum, maximum)
     while not is_prime(p):
@@ -27,34 +30,42 @@ def get_prime(minimum: int, maximum: int) -> int:
 
 
 class RSA:
-    minimum = 100
-    maximum = 999
 
-    def __init__(self):
-        self.p = get_prime(RSA.minimum, RSA.maximum)
-        self.q = get_prime(RSA.minimum, RSA.maximum)
-        self.n = self.p * self.q
-        self.f_of_n = (self.p - 1) * (self.q - 1)
+    def __init__(self, minimum=1000):
+        self.__minimum = minimum
+        self.__maximum = (minimum * 10) - 1
+        self.__p = get_prime(self.__minimum, self.__maximum)
+        self.__q = get_prime(self.__minimum, self.__maximum)
+        self.__n = self.__p * self.__q
+        self.__f_of_n = (self.__p - 1) * (self.__q - 1)
 
-        self.e = self.__calculate_encrypt_key()
-        self.d = self.__calculate_decrypt_key()
-        if self.d < 0:
-            self.d += self.f_of_n
+        self.__e = self.__calculate_encrypt_key()
+        self.__d = self.__calculate_decrypt_key()
 
+    # Finds the encrypt key
     def __calculate_encrypt_key(self) -> int:
-        e = get_random_in_range(RSA.minimum, self.f_of_n)
-        while math.gcd(e, self.f_of_n) != 1:
-            e = get_random_in_range(RSA.minimum, self.f_of_n)
+        e = get_random_in_range(self.__minimum, self.__f_of_n)
+        while math.gcd(e, self.__f_of_n) != 1:
+            e = get_random_in_range(self.__minimum, self.__f_of_n)
         return e
 
     def __calculate_decrypt_key(self) -> int:
-        for d in range(3, self.f_of_n, 2):
-            if (self.e * d) % self.f_of_n == 1:
+        for d in range(3, self.__f_of_n, 2):
+            if (self.__e * d) % self.__f_of_n == 1:
+                if d < 0:
+                    d += self.__f_of_n
                 return d
+
         return -1
 
     def encrypt(self, message):
-        pass
+        ciphertext = []
+        for x in message:
+            ciphertext.append(pow(ord(x), self.__e, self.__n))
+        return ciphertext
 
     def decrypt(self, ciphertext):
-        pass
+        message = ""
+        for x in ciphertext:
+            message += chr(pow(x, self.__d, self.__n))
+        return message
--- src/front_end/application.py
+++ src/front_end/application.py
@@ -4,10 +4,12 @@ from back_end.rsa import *
 
 rsa = RSA()
 
-print(rsa.p)  # large prime, p
-print(rsa.q)  # large prime, q
-print(rsa.e)  # e, part of public key
-print(rsa.d)  # d, part of private key
+message = "RSA IS SECURE. This is a message that is going to be encrypted."
 
+ciphertext = rsa.encrypt(message)
 
-print(rsa.e*rsa.d % rsa.f_of_n == 1)
+decypheredtext = rsa.decrypt(ciphertext)
+
+print(message)
+print(ciphertext)
+print(decypheredtext)
