912216d0b4bae71f2ab309d5e8b0ed8d0f09960c
JamesCZ98 <james.coppock98@gmail.com>
2020-02-07 08:57:14 -0600

    Fixed commit error from previous commit.

--- src/back_end/rsa.py
+++ src/back_end/rsa.py
@@ -3,30 +3,7 @@ import math
 from random import random
 
 
-# Checks if a given number is prime
-def is_prime(number: int) -> bool:
-    if number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0 or number % 11 == 0:
-        return False
-    for i in range(3, 20, 2):
-        k = int(random() * number)
-        if math.gcd(number, k) != 1:
-            return False
-        if k ** (number - 1) % number != 1:
-            return False
-    return True
 
-
-# Returns a random integer within the minimum and maximum, inclusively.
-def get_random_in_range(minimum: int, maximum: int) -> int:
-    return int(minimum + (random() * (maximum - minimum)))
-
-
-# Finds a prime number within a range.
-def get_prime(minimum: int, maximum: int) -> int:
-    p = get_random_in_range(minimum, maximum)
-    while not is_prime(p):
-        p = get_random_in_range(minimum, maximum)
-    return p
 
 
 class RSA:
@@ -34,8 +11,8 @@ class RSA:
     def __init__(self, minimum=1000):
         self.__minimum = minimum
         self.__maximum = (minimum * 10) - 1
-        self.__p = get_prime(self.__minimum, self.__maximum)
-        self.__q = get_prime(self.__minimum, self.__maximum)
+        self.__p = self.__get_prime()
+        self.__q = self.__get_prime()
         self.__n = self.__p * self.__q
         self.__f_of_n = (self.__p - 1) * (self.__q - 1)
 
@@ -44,28 +21,65 @@ class RSA:
 
     # Finds the encrypt key
     def __calculate_encrypt_key(self) -> int:
-        e = get_random_in_range(self.__minimum, self.__f_of_n)
+        e = int(self.__minimum + (random() * (self.__maximum - self.__minimum)))
         while math.gcd(e, self.__f_of_n) != 1:
-            e = get_random_in_range(self.__minimum, self.__f_of_n)
+            e = int(self.__minimum + (random() * (self.__maximum - self.__minimum)))
         return e
 
     def __calculate_decrypt_key(self) -> int:
-        for d in range(3, self.__f_of_n, 2):
-            if (self.__e * d) % self.__f_of_n == 1:
-                if d < 0:
-                    d += self.__f_of_n
-                return d
+        (x, y, d) = self.extended_gcd(self.__e, self.__f_of_n)
+        if y < 0:
+            y += self.__f_of_n
+        return int(y)
 
-        return -1
+    # Checks if a given number is prime
+    def __is_prime(self, number: int) -> bool:
+        if number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0 or number % 11 == 0:
+            return False
+        for i in range(3, 20, 2):
+            k = int(random() * number)
+            if math.gcd(number, k) != 1:
+                return False
+            if k ** (number - 1) % number != 1:
+                return False
+        return True
 
-    def encrypt(self, message):
+    # Finds a prime number within a range.
+    def __get_prime(self) -> int:
+        p = int(self.__minimum + (random() * (self.__maximum - self.__minimum)))
+        while not self.__is_prime(p):
+            p = int(self.__minimum + (random() * (self.__maximum - self.__minimum)))
+        return p
+
+    def extended_gcd(self, a=1, b=1):
+        if a < b:
+            a, b = b, a
+
+        if b == 0:
+            return (1, 0, a)
+        (x, y, d) = self.extended_gcd(b, a % b)
+        return y, x - a // b * y, d
+
+    def encrypt_message(self, message):
         ciphertext = []
         for x in message:
             ciphertext.append(pow(ord(x), self.__e, self.__n))
         return ciphertext
 
-    def decrypt(self, ciphertext):
+    def decrypt_message(self, ciphertext):
         message = ""
         for x in ciphertext:
             message += chr(pow(x, self.__d, self.__n))
+        return message
+
+    def encrypt_signature(self, signature):
+        ciphertext = []
+        for x in signature:
+            ciphertext.append(pow(ord(x), self.__d, self.__n))
+        return ciphertext
+
+    def decrypt_signature(self, ciphertext):
+        message = ""
+        for x in ciphertext:
+            message += chr(pow(x, self.__e, self.__n))
         return message
--- src/front_end/application.py
+++ src/front_end/application.py
@@ -6,10 +6,19 @@ rsa = RSA()
 
 message = "RSA IS SECURE. This is a message that is going to be encrypted."
 
-ciphertext = rsa.encrypt(message)
+ciphertext = rsa.encrypt_message(message)
 
-decypheredtext = rsa.decrypt(ciphertext)
+decypheredtext = rsa.decrypt_message(ciphertext)
 
 print(message)
 print(ciphertext)
 print(decypheredtext)
+
+
+signature = "My Name"
+encrypted_sig = rsa.encrypt_signature(signature)
+decrypted_sig = rsa.decrypt_signature(encrypted_sig)
+
+print(signature)
+print(encrypted_sig)
+print(decrypted_sig)
