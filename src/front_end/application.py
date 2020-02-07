853e854f68623637f2d9cbaa8b9ec5063d80e618
Chase Engdahl <cengdahl1@cub.uca.edu>
2020-02-07 10:03:12 -0600

    Front end

--- src/front_end/application.py
+++ src/front_end/application.py
@@ -4,21 +4,68 @@ from back_end.rsa import *
 
 rsa = RSA()
 
-message = "RSA IS SECURE. This is a message that is going to be encrypted."
+#front end
 
-ciphertext = rsa.encrypt_message(message)
+rsa = RSA()
 
-decypheredtext = rsa.decrypt_message(ciphertext)
+def main():
+    print("Do you want to ")
+    print("1. Encrypt or decrypt a message or ")
+    print("2. Decipher or sign a document digitally?")
+    print("Enter 1 or 2: ")
+    encrypt_or_sign = int(input())
+    print("Are you sending or recieving message?")
+    print("1. Sending")
+    print("2. Recieveing")
+    print("Enter 1 or 2: ")
+    send_or_recieve = int(input())
 
-print(message)
-print(ciphertext)
-print(decypheredtext)
+    if encrypt_or_sign == 1 and send_or_recieve == 1:
+        encrypt_sender()
+    elif encrypt_or_sign == 1 and send_or_recieve == 2:
+        encrypt_reciever()
+    elif encrypt_or_sign == 2 and send_or_recieve == 1:
+        sign_sender()
+    elif encrypt_or_sign == 2 and send_or_recieve == 2:
+        sign_reciever()
+    
+
+    
+def encrypt_sender():
+    #encrypts message
+    print("Input the message you want to send:")
+    message = input()
+
+    encryptedMessage = rsa.encrypt_message(message)
+    
+    print("Encrypted message is: ")
+    print(message)
 
 
-signature = "My Name"
-encrypted_sig = rsa.encrypt_signature(signature)
-decrypted_sig = rsa.decrypt_signature(encrypted_sig)
+def encrypt_reciever():
+    #decrypts message
+    print("Enter the encrypted messsage:")
+    encrypted_mes = input()
 
-print(signature)
-print(encrypted_sig)
-print(decrypted_sig)
+    decryptedMessage = rsa.decrypt_message(encrypted_mes)
+    
+    print("The deciphered message is: ")
+    print(decryptedMessage)
+    
+def sign_sender():
+    #sign digital document
+    print("Enter file name to be signed:")
+    filename = input ("Filename: ");
+    with open (filename, "a") as f:
+        f.write (input ());
+    
+    
+    
+
+
+def sign_reciever():
+    #decipher digital signature
+    print("Enter file name to be read:")
+   
+    
+
